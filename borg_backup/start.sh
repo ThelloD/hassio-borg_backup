60531447f7fa2c33b30ea877d9dcc0ed45077666
ThelloD <ThelloD@users.noreply.github.com>
2022-02-17 01:51:56 +0100

    Update start.sh

--- borg_backup/start.sh
+++ borg_backup/start.sh
@@ -110,9 +110,10 @@ local_snapshot=$((local_snapshot_config + 1))
 if [ $local_snapshot -gt 1 ]; then
   bashio::log.info 'Cleaning old snapshots.'
   cd /backup
-  ls -tp | grep -v '/$' | tail -n +$local_snapshot | tr '\n' '\0' | xargs -0 rm -- || true
   # Debugging
   ls -tp | grep -v '/$' | tail -n +$local_snapshot | cat -
+  # Delete
+  ls -tp | grep -v '/$' | tail -n +$local_snapshot | tr '\n' '\0' | xargs -0 rm -- || true
 fi
 
 bashio::log.info 'Finished.'
